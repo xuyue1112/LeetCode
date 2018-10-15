06499893f5d1d14b7ecb5f4525e9bb5a7059575a
徐越 <xuyue@xuyue-mac.local>
2018-10-15 18:47:04 +0800

    236

---
+++ 236.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL || root==p || root==q){
+            return root;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        return left? right? root:left : right; //right? right: right
+    }
+};
